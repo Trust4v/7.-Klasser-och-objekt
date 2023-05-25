c8c2f05f2176de33a3e1bf7e26e3de19829bc6f4
Gustav Sommar <gustav.ludvig.sommar@skola.botkyrka.se>
2023-05-25 13:36:53 +0200

    Add project files.

---
+++ Uppgift 1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tärning tärning = new Tärning();
+
+            while (true)
+            {
+                Console.WriteLine("Välkommen till tärningskastarprogramet! \r\nSkriv in spelare 1 följt av spelare 2 och sen kommer ni få ett poäng beroende på tärningsvärdet");
+                Console.Write("Namn 1:");
+                string namn1 = Console.ReadLine();
+                Console.Write("Namn 2:");
+                string namn2 = Console.ReadLine();
+                int värde = tärning.Kasta();
+                Console.WriteLine($"{namn1} fick värdet " + värde);
+                int värde2 = tärning.Kasta();
+                Console.WriteLine($"{namn2} fick värde " + värde2);
+                if (värde == värde2)
+                {
+                    Console.WriteLine("Det blev lika");
+                }
+                else if (värde > värde2)
+                {
+                    Console.WriteLine($"{namn1} Vann");
+                }
+                else
+                {
+                    Console.WriteLine($"{namn2} Vann");
+                }
+                Console.WriteLine("Spela igen? (Ja/Nej)");
+                string val = Console.ReadLine();
+                if (val.ToLower() == "ja")
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    break;                   
+                }
+            }
+        }
+
+        class Tärning
+        {
+            //Medlemsvariabler
+            int värde;
+            Random rnd = new Random();
+            //Metoder      
+            public Tärning()
+            {
+                värde = 1;
+            }
+            public int Kasta()
+            {
+                return värde = rnd.Next(1, 7);
+            }
+            public int Värde
+            {
+                get { return värde; }
+            }
+        }
+    }
+}
---
+++ Uppgift 2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till bank. Skriv ditt namn för att skapa ett konto:");
+            string namn = Console.ReadLine();
+            BankKonto konto1 = new BankKonto(namn);
+
+            while (true)
+            {
+                Console.WriteLine("Vad vill du göra? \r\n1)Sätt in pengar på konto\r\n2)Ta ut pengar\r\n3)Kolla pengar nivåer\r\n4)Avsluta");
+                try
+                {
+                    int val = int.Parse(Console.ReadLine());
+                    if (val==1)
+                    {
+                        Console.WriteLine("Hur mycket pengar vill du sätta in?");
+                        int temp = int.Parse(Console.ReadLine());
+                        konto1.SättaIn(temp);
+                    }
+                    else if (val ==2)
+                    {
+                        Console.WriteLine("Hur mycket pengar vill du ta ut?");
+                        int temp = int.Parse(Console.ReadLine());
+                        konto1.TaUt(temp);
+                    }
+                    else if (val == 3)
+                    {
+                        Console.WriteLine(konto1.ToString());
+                        Console.ReadLine();
+                    }
+                    if (val == 4)
+                    {
+                        break;
+                    }
+                }
+                catch
+                {
+
+                    throw;
+                }
+                Console.Clear();
+            }
+        }
+    }
+    class BankKonto
+    {
+        string kontoNamn;
+        int pengar;
+        public BankKonto(string n)
+        {
+            kontoNamn = n;
+        }
+        public void SättaIn(int p)
+        {
+            pengar += p;
+        }
+        public void TaUt(int p)
+        {
+            pengar -= p;
+        }
+        public override string ToString()
+
+        {
+            return "Kontot som ägs av " + kontoNamn + " har " + pengar + " kronor";
+        }
+
+    }
+}
