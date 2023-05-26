f531c1b5370989b906e4222edfef26ef53b64eca
Gustav Sommar <gustav.ludvig.sommar@skola.botkyrka.se>
2023-05-26 15:42:49 +0200

    s

---
+++ Poker/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            KortLek kortlek = new KortLek(1);
+            Spelare Gustav = new Spelare("Gustav");
+            kortlek.Blanda();
+            for (int i = 0; i < 5; i++)
+            {
+                Gustav.DraKort(kortlek.DraKort()); 
+            }
+            Gustav.KollaHand();
+        }
+        
+    }
+
+    enum Färg
+    {
+        Hjärter = 1,
+        Spader = 2,
+        Ruter = 3,
+        Klöver = 4
+    }
+    class Kort
+    {
+        //Medlemsvariabler
+        int valör; //2-E
+        Färg färg; //"ruter"
+        //Kompilator
+        public Kort(Färg f, int v)
+        {
+            valör = v;
+            färg = f;
+        }
+        private string ValörFix(int v)
+        {            
+            if (v == 11)
+            {
+                return "Kn";
+            }
+            if (v == 12)
+            {
+                return "D";
+            }
+            if (v == 13)
+            {
+                return "K";
+            }
+            if (v == 14)
+            {
+                return "E";
+            }
+            else
+            {
+                return v.ToString();
+            }
+        }
+        public override string ToString()
+        {
+            return färg + " " + ValörFix(valör);
+        }
+
+    }
+    class KortLek
+    {
+        //medlemsvariabler
+        List<Kort> kortlek = new List<Kort>();
+        int kortILeken=0;
+        int antalKortlekar = 1;
+        Random rnd = new Random();
+        
+        //konstruktor
+        public KortLek(int ak)
+        {
+            antalKortlekar = ak;
+            for (int y = 0; y < ak; y++)
+            {
+                for (int i = 1; i < 5; i++)
+                {
+                    for (int j = 2; j < 15; j++)
+                    {
+                        Kort temp = new Kort((Färg)i, j);
+                        kortlek.Add(temp);
+                    }
+                } 
+            }
+        }
+        //Metoder
+        public void Blanda()
+        {
+            kortILeken = 0;
+            int n = kortlek.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                Kort temp = kortlek[k];
+                kortlek[k] = kortlek[n];
+                kortlek[n] = temp;
+            }
+        }
+        public void BlandaOm()
+        {
+            kortlek.Clear();
+            for (int y = 0; y < antalKortlekar; y++)
+            {
+                for (int i = 1; i < 5; i++)
+                {
+                    for (int j = 2; j < 15; j++)
+                    {
+                        Kort temp = new Kort((Färg)i, j);
+                        kortlek.Add(temp);
+                    }
+                }
+            }
+            kortILeken = 0;
+            int n = kortlek.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                Kort temp = kortlek[k];
+                kortlek[k] = kortlek[n];
+                kortlek[n] = temp;
+            }
+        }
+        public void SkrivUt()
+        {
+            foreach (Kort kort in kortlek)
+            {
+                Console.WriteLine(kort);
+            }
+        }
+        internal Kort DraKort()
+        {
+            Kort returVärde = kortlek.ElementAt(0);
+            kortlek.RemoveAt(0);
+            return returVärde;
+        }
+        //ToString 
+        public override string ToString()
+        {
+            return "Kortleken innehåller " + kortlek.Count + " kort just nu.";
+        }
+
+        
+
+        //Egenskaper
+        public Kort Tjuvkika
+        {
+            get { return kortlek[kortILeken-1]; }
+        }
+    }
+    #region wip
+    /*class AntalChips
+{
+   string kontoNamn;
+   int pengar;
+   public AntalChips(string n)
+   {
+       kontoNamn = n;
+   }
+   public void SättaIn(int p)
+   {
+       if (p < 0)
+       {
+           pengar += p;
+       }
+   }
+   public void Betta(int p)
+   {
+       if (p < pengar)
+       {
+           pengar -= p;
+       }
+       else
+       {
+       }
+   }
+   public void KollaSaldo()
+   {
+       Console.WriteLine("Du har " + pengar + " chips");
+   }
+   public override string ToString()
+
+   {
+       return kontoNamn + " har " + pengar + " chips";
+   }
+
+}*/ 
+    #endregion
+    class Spelare
+    {
+        //medlemsvariabler
+        string namn;
+        List<Kort> hand = new List<Kort>();
+        int chips;
+        //konstruktro
+        public Spelare(string n)
+        {
+            namn = n;
+        }
+        //Metoder
+        public void DraKort(Kort kort)
+        {
+            hand.Add(kort);
+        }
+        public void KollaHand()
+        {
+            foreach (Kort kort in hand)
+            {
+                Console.Write(kort+ ", ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Uppgift 3/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_3
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            int[] kort1A = new int[2];
+            int[] kort2A = new int[2];
+            Console.WriteLine("Välkommen till Dra kort pogram och tävling \r\nSkriv in två namn");
+            Console.Write("Namn 1:");
+            string namn1 = Console.ReadLine();
+            Console.Write("Namn 2:");
+            string namn2 = Console.ReadLine();
+            Kort kort1 = new Kort();
+            Kort kort2 = new Kort();
+            kort1.DraKort();
+            kort2.DraKort();
+            for (int i = 0; i < 2; i++)
+            {
+                kort1A[i] = kort1.Värde[i];
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                kort2A[i] = kort2.Värde[i];
+            }
+            bool storlek = kort1.TestaStorlek(kort1A, kort2A);
+            if (storlek)
+            {
+                Console.WriteLine(namn1 + " vann med " + kort1.ToString());
+                Console.WriteLine(namn2 + " hade " + kort2.ToString());
+            }
+            else
+            {
+                Console.WriteLine(namn2 + " vann med " + kort2.ToString());
+                Console.WriteLine(namn1 + " hade " + kort1.ToString());
+            }
+        }
+    }
+    
+    class Kort
+    {
+        //Medlems Variablerana
+        int[] kort = new int[2] {1,1};
+        public int speed = 1;
+        public Random rnd = new Random(speed);
+        //Metoder
+        public void DraKort()
+        {
+            kort[0] = rnd.Next(1, 5);
+            kort[1] = rnd.Next(1, 14);
+        }
+        public Kort(int t)
+        {
+            speed = t;
+        }
+        public bool TestaStorlek(int[] kort1, int[] kort2)
+        {
+            bool StörreEllerMindre = false;
+            if (kort1[1]>kort2[1])
+            {
+                StörreEllerMindre = true;
+            }
+            if (kort1[1]==kort2[1])
+            {
+                if (kort1[0] < kort2[0])
+                {
+                    StörreEllerMindre = true;
+                }
+            }
+            return StörreEllerMindre;
+        }
+        public override string ToString()
+        {
+            if (kort[0] == 1)
+            {
+                return "Härter "+ kort[1]; 
+            }
+            if (kort[0] == 2)
+            {
+                return "Spader " + kort[1];
+            }
+            if (kort[0] == 3)
+            {
+                return "Ruter " + kort[1];
+            }
+            if (kort[0] == 4)
+            {
+                return "Klöver " + kort[1];
+            }
+            else
+            {
+                return "Dinmamma";
+            }
+        }
+        public int[] Värde
+        {
+            get { return kort; }
+        }
+
+    }
+}
